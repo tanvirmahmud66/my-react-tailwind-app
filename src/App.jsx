a7d5b8fd5f3fdb1ac3a957226a5027f0241f581d
Tanvir Mahmud <tanvirmahmudfahim1313@gmail.com>
2025-09-10 11:24:53 +0600

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import ProjectDetails from './pages/ProjectDetails';
-import Footer from './components/Footer';
+
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ProjectDetails from "./pages/ProjectDetails";
+import Footer from "./components/Footer";
+
+import { Infinity } from "ldrs/react";
+import "ldrs/react/Infinity.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading for 2 seconds (you can adjust)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-[#0B192C]">
+        <Infinity
+          size="55"
+          stroke="4"
+          strokeLength="0.15"
+          bgOpacity="0.1"
+          speed="1.3"
+          color="white"
+        />
+      </div>
+    );
+  }
+
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-[#0B192C]">
         <Navbar />
         <main className="flex-grow">
           <Routes>
